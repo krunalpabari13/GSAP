535ef6c1c4f739345ceb3c32f234048a13607bab
krunal <krunalpabari13>
2023-06-10 21:43:52 +0530

    final

--- main.js
+++ main.js
@@ -1,57 +1,5 @@
     gsap.registerPlugin(ScrollTrigger);
-  
-    // gsap.from('.box1', {
-         
-    //   scrollTrigger: {
-    //     trigger: '.box3',
-    //     start: 'top top', // Adjust the start position as needed
-    //     end: 'bottom 20%', // Adjust the end position as needed
-    //     scrub: true, 
-    //     markers: {startColor: "black", endColor: "black", fontSize: "18px", fontWeight: "bold", indent: 20}
-    //   },x: 400,
-      
-    //   duration:3,
-    // }).to('.box1',{rotation:300});
-  
-// let tl=gsap.timeline({
-//     scrollTrigger:{
-//         trigger:'.maincontainer',
-//         start:'top top',
-//         end:"bottom top",
-//         scrub:true,
-//         pin:true,
-//         pinSpacing:false,
-//         markers:  {startColor: "black", endColor: "black", fontSize: "18px", fontWeight: "bold", indent: 20}
-//      }
-// })
-
-// // tl.to('.box2',{top:'100%'},'a');
-// // tl.to('.box3',{top:'0%'},'');
-// // tl.to('.box1',{top:'-70%'},'c');
-// tl.from('.box2',{y:0,opacity:1},'a');
-// tl.to('.box2',{y:-600,opacity:1},'a');
-// tl.from('.box3',{y:50,opacity:1},'a');
-// tl.to('.box3',{y:-600,opacity:1},'a');
-// tl.from('.box1',{y:1000,opacity:1},'a');
-// tl.to('.box1',{y:-600,opacity:1},'a');
-
-
-//page slidding working
-// let tl=gsap.timeline()
-
-// gsap.utils.toArray(".box").forEach((box,i)=>{
-//     ScrollTrigger.create({
-//         animation:tl,
-//         trigger: box,
-//         start:'top top',
-//         end:"+=2000",
-//         pin :true,
-//         scrub:true,
-//         pinSpacing:false,
-//         markers:{startColor: "black", endColor: "black", fontSize: "18px", fontWeight: "bold", indent: 20}
-//     }
-//     )
-// })
+ 
 let t1=gsap.timeline()
 ScrollTrigger.create({
     animation:t1,
@@ -64,10 +12,13 @@ ScrollTrigger.create({
     // scrub:true,
     markers:{startColor: "black", endColor: "black", fontSize: "18px", fontWeight: "bold", indent: 20}
 })
-t1.from('#nexgtv1', { duration: 1.2, ease: "power4.out", y: -500 },'b');
-t1.to('#nexgtv1', { opacity:1 },'b');
-t1.from('#nexgtv2', { duration: 1.2, ease: "power4.out", y: 1500 },'b');
-t1.to('#nexgtv2', { opacity:1 },'b');
+t1.to(".text",{visibility:'hidden'})
+t1.to("#text1",{visibility:'visible'})
+t1.to(".leftpanel,.round",{backgroundColor:'rgb(88, 211, 218)'})
+t1.from('#nexgtv1', {  ease: "power2.out", y: -500 ,immediateRender: true},'b');
+t1.to('#nexgtv1', { opacity:1 ,immediateRender: true},'b');
+t1.from('#nexgtv2', {  ease: "power4.out", y: 1500 ,immediateRender: true},'b');
+t1.to('#nexgtv2', { opacity:1,immediateRender: true },'b');
 
 let t2=gsap.timeline()
 ScrollTrigger.create({
@@ -81,10 +32,14 @@ ScrollTrigger.create({
     // scrub:true,
     markers:{startColor: "black", endColor: "black", fontSize: "18px", fontWeight: "bold", indent: 20}
 })
-t2.from('#blockchain1', { duration: 1.2, ease: "power4.out", y: -500 },'b');
-t2.to('#blockchain1', { opacity:1 },'b');
-t2.from('#blockchain2', { duration: 1.2, ease: "power4.out", y: 1500 },'b');
-t2.to('#blockchain2', { opacity:1 },'b');
+t2.to(".text",{visibility:'hidden'})
+t2.to("#text2",{visibility:'visible'})
+t2.to(".leftpanel,.round",{backgroundColor:'rgb(54, 54, 91)'})
+t2.to("#Dots2, #Opaque_Ring",{strokeDasharray:'129,1000'})
+t2.from('#blockchain1', {  ease: "power4.out", y: -500,immediateRender: true,delay:0 },'b');
+t2.to('#blockchain1', { opacity:1 ,immediateRender: true,delay:0},'b');
+t2.from('#blockchain2', {  ease: "power4.out", y: 1500 ,immediateRender: true,delay:0},'b');
+t2.to('#blockchain2', { opacity:1,immediateRender: true,delay:0 },'b');
 
 
 
@@ -101,16 +56,24 @@ ScrollTrigger.create({
     // scrub:true,
     markers:{startColor: "black", endColor: "black", fontSize: "18px", fontWeight: "bold", indent: 20}
 })
-tl.from('#dombox', { duration: 1.2, ease: "power4.out", y: -500 },'a');
+tl.to(".text",{visibility:'hidden'})
+
+tl.to("#text4",{visibility:'visible'})
+tl.to(".leftpanel,.round",{backgroundColor:'rgb(121, 86, 130)'})
+
+tl.to("#Dots4, #Opaque_Ring",{strokeDasharray:'409,1000'})
+tl.from('#dombox', {  ease: "power4.out", y: -500 },'a');
 tl.to('#dombox', { opacity:1 },'a');
-tl.from('#dommob1', { duration: 1.2, ease: "power4.out", y: 1500 },'a');
+tl.from('#dommob1', {  ease: "power4.out", y: 1500 },'a');
 tl.to('#dommob1', { opacity:1 },'a');
-tl.from('#dommob2', { duration: 1.2, ease: "power4.out", y: -1000 },'a');
+tl.from('#dommob2', {  ease: "power4.out", y: -1000 },'a');
 tl.to('#dommob2', { opacity:1 },'a');
-tl.from('#bun1', { duration: 1.2, ease: "power4.out", y: -500 },'a');
+tl.from('#bun1', {  ease: "power4.out", y: -500 },'a');
 tl.to('#bun1', { opacity:1 },'a');
-tl.from('#bun2', { duration: 1.2, ease: "power4.out", y: 1000 },'a');
+tl.from('#bun2', {  ease: "power4.out", y: 1000 },'a');
 tl.to('#bun2', { opacity:1 },'a');
+
+
 
 let tl2=gsap.timeline();
 ScrollTrigger.create({
@@ -124,8 +87,14 @@ ScrollTrigger.create({
     // scrub:true,
     markers:{startColor: "black", endColor: "black", fontSize: "18px", fontWeight: "bold", indent: 20}
 })
-tl2.to('#nasafitness1', { duration: 2.2, ease: "power4.out", y: 10 ,opacity:1},'c');
-tl2.to('#nasafitness2', { duration: 2.2, ease: "power4.out", y: -200 ,opacity:1},'c');
+tl2.to(".text",{visibility:'hidden'})
+
+tl2.to("#text3",{visibility:'visible'})
+tl2.to(".leftpanel,.round",{backgroundColor:'rgb(113, 113, 201)'})
+
+tl2.to("#Dots3, #Opaque_Ring",{strokeDasharray:'272,1000'})
+tl2.to('#nasafitness1', {  ease: "power4.out", y: 10 ,opacity:1},'c');
+tl2.to('#nasafitness2', {  ease: "power4.out", y: -200 ,opacity:1},'c');
 
 
 
@@ -141,16 +110,21 @@ ScrollTrigger.create({
     // scrub:true,
     markers:{startColor: "black", endColor: "black", fontSize: "18px", fontWeight: "bold", indent: 20}
 })
-t3.from('#galmbo1', { duration: 1.2, ease: "power4.out", y: -500 },'a');
+t3.to(".text",{visibility:'hidden'})
+
+t3.to("#text5",{visibility:'visible'})
+t3.to(".leftpanel,.round",{backgroundColor:'rgb(88, 88, 168)'})
+t3.to("#Dots5, #Opaque_Ring",{strokeDasharray:'528,1000'})
+t3.from('#galmbo1', {  ease: "power4.out", y: -500 },'a');
 t3.to('#galmbo1', { opacity:1},'a');
-t3.from('#galmob2', { duration: 1.2, ease: "power4.out", y: 1500 },'a');
+t3.from('#galmob2', {  ease: "power4.out", y: 1500 },'a');
 t3.to('#galmob2', { opacity:1 },'a');
-t3.from('#galmob3', { duration: 1.2, ease: "power4.out", y: -1000 },'a');
+t3.from('#galmob3', {  ease: "power4.out", y: -1000 },'a');
 t3.to('#galmob3', { opacity:1 },'a');
-t3.from('#galmob4', { duration: 1.2, ease: "power4.out", y: -500 },'a');
+t3.from('#galmob4', {  ease: "power4.out", y: -500 },'a');
 t3.to('#galmob4', { opacity:1 },'a');
-t3.from('#galmob5', { duration: 1.2, ease: "power4.out", y: 1000 },'a');
-t3.to('#galmob5', { opacity:1,y:-1300 },'a');
+t3.from('#galmob5', { duration:1.2, ease: "power4.out", y: 1000 },'a');
+t3.to('#galmob5', { opacity:1 },'a');
 
 
 
@@ -167,8 +141,14 @@ ScrollTrigger.create({
     // scrub:true,
     markers:{startColor: "black", endColor: "black", fontSize: "18px", fontWeight: "bold", indent: 20}
 })
-t4.to('#erp', { duration: 2.2, ease: "power4.out", y: 10 ,opacity:1},'c');
-t4.to('#erp', { duration: 2.2, ease: "power4.out", y: -200 ,opacity:1},'c');
+t4.to(".text",{visibility:'hidden'})
+
+t4.to("#text6",{visibility:'visible'})
+t4.to(".leftpanel,.round",{backgroundColor:'rgb(137, 119, 85)'})
+t4.to("#Dots6, #Opaque_Ring",{strokeDasharray:'668,1000'})
+
+t4.to('#erp', {  ease: "power4.out", y: 10 ,opacity:1});
+// t4.to('#erp', {  ease: "power4.out", y: -200 ,opacity:1});
 
 let t5=gsap.timeline();
 ScrollTrigger.create({
@@ -182,33 +162,14 @@ ScrollTrigger.create({
     // scrub:true,
     markers:{startColor: "black", endColor: "black", fontSize: "18px", fontWeight: "bold", indent: 20}
 })
-t5.to('#melto1', { duration: 2.2, ease: "power4.out", y: -200 ,opacity:1},'c');
-t5.to('#melto2', { duration: 2.2, ease: "power4.out", y: -200 ,opacity:1},'c');
+t5.to(".text",{visibility:'hidden'})
 
-// Animation for the first page
-// gsap.from('.box3', {
-//   opacity: 0,
-//   duration: 1,
-//   scrollTrigger: {
-//     animation:tl,
-//     toggleActions:'restart reset restart none',
-//     trigger: '.box3',
-//     start: 'top top',
-//     end: 'bottom top',
-//     scrub: true
-//   }
-// });
+t5.to("#text7",{visibility:'visible'})
+t5.to(".leftpanel,.round",{backgroundColor:'rgb(154, 84, 84)'})
 
-// // Animation for the second page
-// gsap.from('.box1', {
-//   opacity: 0,
-//   duration: 1,
-//   scrollTrigger: {
-//     animation:tl2,
-//     toggleActions:'restart none restart reset',
-//     trigger: '.box1',
-//     start: 'top top',
-//     end: 'top bottom',
-//     // scrub: true
-//   }
-// });
+t5.to("#Dots7, #Opaque_Ring",{strokeDasharray:'804,1000'})
+t5.to('#melto1', {  ease: "power4.out", y: -200 ,opacity:1},'c');
+t5.to('#melto2', {  ease: "power4.out", y: -200 ,opacity:1},'c');
+
+
+
